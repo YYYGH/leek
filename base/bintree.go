0422f493d43d2ca1a4dd2aa9eb9a06ab71c71b46
YYYGH <yyznt@163.com>
2021-05-30 11:48:45 +0800

    在二叉树节点增加节点计数字段

--- base/bintree.go
+++ base/bintree.go
@@ -5,6 +5,8 @@ type BinTree struct {
 	Data  interface{}
 	Left  *BinTree
 	Right *BinTree
+	// 以当前节点为根节点的树的总结点数
+	Count int
 }
 
 func NewBinTreeNode(data interface{}, left, right *BinTree) *BinTree {
