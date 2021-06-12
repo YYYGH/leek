4d2e147f71c62ecdfbc0e9b5a745b34b7930355b
YYYGH <yyznt@163.com>
2021-06-12 21:24:02 +0800

    二分搜索元素和元素左右区间

---
+++ leetcode/bs/bs.go
@@ -0,0 +1,138 @@
+package bs
+
+func BinarySearch(list []int, target int) int {
+	if len(list) == 0 {
+		return -1
+	}
+
+	left := 0
+	right := len(list) - 1
+	// [left, right]
+	for left <= right {
+		mid := left + (right-left)/2
+		if list[mid] == target {
+			return mid
+		} else if list[mid] < target {
+			left = mid + 1
+		} else if list[mid] > target {
+			right = mid - 1
+		}
+	}
+	return -1
+}
+
+func BinarySearchV2(list []int, target int) int {
+	if len(list) == 0 {
+		return -1
+	}
+
+	left := 0
+	right := len(list)
+	// [left, right)
+	for left < right {
+		mid := left + (right-left)/2
+		if list[mid] == target {
+			return mid
+		} else if list[mid] < target {
+			left = mid + 1
+		} else if list[mid] > target {
+			right = mid
+		}
+	}
+	return -1
+}
+
+// 查找左边界
+func BinarySearchLeftRound(list []int, target int) int {
+	if len(list) == 0 {
+		return -1
+	}
+
+	left := 0
+	right := len(list) - 1
+	// [left, right]
+	for left <= right {
+		mid := left + (right-left)/2
+		if list[mid] == target {
+			right = mid - 1
+		} else if list[mid] < target {
+			left = mid + 1
+		} else if list[mid] > target {
+			right = mid - 1
+		}
+	}
+	if left >= len(list) || list[left] != target {
+		return -1
+	}
+	return left
+}
+
+func BinarySearchLeftRoundV2(list []int, target int) int {
+	if len(list) == 0 {
+		return -1
+	}
+	left := 0
+	right := len(list)
+	// [left, right)
+	for left < right {
+		mid := left + (right-left)/2
+		if list[mid] == target {
+			right = mid
+		} else if list[mid] < target {
+			left = mid + 1
+		} else if list[mid] > target {
+			right = mid
+		}
+	}
+	if left >= len(list) || list[left] != target {
+		return -1
+	}
+	return left
+}
+
+func BinarySearchRightRound(list []int, target int) int {
+	if len(list) == 0 {
+		return -1
+	}
+	left := 0
+	right := len(list) - 1
+	// [left, right]
+	for left <= right {
+		mid := (right-left)>>1 + left
+		if list[mid] == target {
+			left = mid + 1
+		} else if list[mid] < target {
+			left = mid + 1
+		} else if list[mid] > target {
+			right = mid - 1
+		}
+	}
+	if right < 0 || list[right] != target {
+		return -1
+	}
+	return right
+}
+
+func BinarySearchRightRoundV2(list []int, target int) int {
+	if len(list) == 0 {
+		return -1
+	}
+
+	left := 0
+	right := len(list)
+	// [left, right)
+	for left < right {
+		mid := (right-left)>>1 + left
+		if list[mid] == target {
+			left = mid + 1
+		} else if list[mid] < target {
+			left = mid + 1
+		} else if list[mid] > target {
+			right = mid
+		}
+	}
+	if left == 0 || list[right-1] != target {
+		return -1
+	}
+	return right - 1
+}
---
+++ leetcode/bs/bs_test.go
@@ -0,0 +1,82 @@
+package bs
+
+import "testing"
+
+var (
+	dataBS = map[int][][]int{
+		3:  {{8}, {2, 3, 5, 8, 9}},
+		2:  {{3}, {1, 2, 3, 4, 5}},
+		0:  {{2}, {2, 3, 5, 8, 9}},
+		-1: {{6}, {2, 3, 5, 8, 9}},
+	}
+
+	dataLeftRound = map[int][][]int{
+		3:  {{8}, {2, 3, 5, 8, 8, 8, 9, 10}},
+		1:  {{3}, {1, 3, 3, 4, 5}},
+		0:  {{2}, {2, 3, 5, 8, 9}},
+		4:  {{9}, {2, 3, 5, 8, 9}},
+		-1: {{6}, {2, 3, 5, 8, 9}},
+	}
+
+	dataRightRound = map[int][][]int{
+		5:  {{8}, {2, 3, 5, 8, 8, 8, 9, 10}},
+		2:  {{3}, {1, 3, 3, 4, 5}},
+		0:  {{2}, {2, 3, 5, 8, 9}},
+		4:  {{9}, {2, 3, 5, 8, 9}},
+		-1: {{6}, {2, 3, 5, 8, 9}},
+		// -1: {{10}, {2, 3, 5, 8, 9}},
+	}
+)
+
+func TestBinarySearch(t *testing.T) {
+	for k, v := range dataBS {
+		index := BinarySearch(v[1], v[0][0])
+		if index != k {
+			t.Errorf("expect: %d, actual: %d, s:%d, v: %v", k, index, v[0][0], v[1])
+		}
+	}
+}
+
+func TestBinarySearchV2(t *testing.T) {
+	for k, v := range dataBS {
+		index := BinarySearchV2(v[1], v[0][0])
+		if index != k {
+			t.Errorf("expect: %d, actual: %d, s:%d, v: %v", k, index, v[0][0], v[1])
+		}
+	}
+}
+
+func TestBinarySearchLeftRound(t *testing.T) {
+	for k, v := range dataLeftRound {
+		index := BinarySearchLeftRound(v[1], v[0][0])
+		if index != k {
+			t.Errorf("expect: %d, actual: %d, s:%d, v: %v", k, index, v[0][0], v[1])
+		}
+	}
+}
+
+func TestBinarySearchLeftRoundV2(t *testing.T) {
+	for k, v := range dataLeftRound {
+		index := BinarySearchLeftRoundV2(v[1], v[0][0])
+		if index != k {
+			t.Errorf("expect: %d, actual: %d, s:%d, v: %v", k, index, v[0][0], v[1])
+		}
+	}
+}
+
+func TestBinarySearchRightRound(t *testing.T) {
+	for k, v := range dataRightRound {
+		index := BinarySearchRightRound(v[1], v[0][0])
+		if index != k {
+			t.Errorf("expect: %d, actual: %d, s:%d, v: %v", k, index, v[0][0], v[1])
+		}
+	}
+}
+func TestBinarySearchRightRoundV2(t *testing.T) {
+	for k, v := range dataRightRound {
+		index := BinarySearchRightRoundV2(v[1], v[0][0])
+		if index != k {
+			t.Errorf("expect: %d, actual: %d, s:%d, v: %v", k, index, v[0][0], v[1])
+		}
+	}
+}
